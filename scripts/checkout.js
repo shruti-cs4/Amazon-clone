2ff7c044ef41050064628bd2b43ae8677cf07b22
shruti-cs4 <shrutichandravanshi04@gmail.com>
2025-11-08 21:42:57 +0530

    updating-checkout

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,12 +4,49 @@ import {products} from "../data/productData.js";
 
 let cartHTML = '';
 
+const headerHTML = `
+    <div class="left-section">
+      <a class="homepage-link" href="amazon.html">
+      <img src="images/icons/amazon-logo.png" class="amazon-logo">
+      </a>
+    </div>
+    <div class="middle-section">
+      <span>Checkout(
+      <span class="header-item"> ${updateCartQuantity()} items </span>
+      )</span>
+    </div>
+    <div class="right-section">
+      <img src="images/icons/checkout-lock-icon.png">
+    </div>
+  `;
 
 const orderHTML = `
+  <div class="js-order-summary">
     <div class="order-heading">Order summary</div>
-    <div class="Items">
-      <div>Items(${updateCartQuantity()}): </div>
+    <div class="items">
+      <div class="items-text">Items(${updateCartQuantity()}): </div>
+      <div class="items-value">$40.99</div>
     </div>
+    <div class="shipping-charges">
+      <div class="shipping-charges-text">Shipping & Handling: </div>
+      <div class="SC-value">$0.00</div>
+    </div>
+    <div class="total-B-tax">
+      <div class="total-B-tax-text">Total Before Tax: 
+      </div>
+      <div class="TBT-value">$40.99</div> 
+    </div>
+    <div class="E-tax">
+      <div class="E-tax-text">Estimated Tax: </div>
+      <div class="ET-value">$3.60</div>
+    </div>
+    <div class="order-total">
+      <div class="order-total-text">Order total: </div>
+      <div class="OT-value"> $44.59</div>
+      </div>
+    <button class="order-button">Place your order</button>
+  </div>
+
 
 `;
 
@@ -85,6 +122,6 @@ cart.forEach((cartItem) => {
   cartHTML += html1;
 });
 
-
-document.querySelector('.js-order-summary').innerHTML = orderHTML;
+document.querySelector('.header').innerHTML = headerHTML;
+document.querySelector('.sidebar').innerHTML = orderHTML;
 document.querySelector('.js-cartItem-grid').innerHTML = cartHTML;
